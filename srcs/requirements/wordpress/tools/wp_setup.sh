fd4a57939f80c33ae5bacd790bb35c297e0d2724
Tatamize <news@interest-a.com>
2023-12-17 08:56:51 +0100

    delete ARG from WordPress Dockerfile, change wp_setup.sh for set-up

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -2,6 +2,8 @@
 
 if ! $(wp core is-installed --allow-root); then
 
+    sed -i -e "s/ARG_DB/$ARG_DB/" -e "s/ARG_USER/$ARG_USER/" -e "s/ARG_PASS_SQL/$ARG_PASS_SQL/" /var/www/html/wp-config.php \
+
     wp core install --allow-root \
     --url="$ARG_DOMAIN" \
     --title="$WP_TITLE" \
